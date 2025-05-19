7e3708fc9ee9d75580992a5c1dff800374e7f295
Divij <diviaops@gmail.com>
2025-05-18 21:47:02 -0500

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-    './dist/**/*.{html,js}',
-    './*.{html,js}',
-    './node_modules/flowbite/**/*.js',
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./../dist/**/*.{html,js}",
+    "./*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
     fontFamily: {
-      'sans': 'Inter, sans-serif',
-      'serif': 'Libre Baskerville, serif',
-    }
+      sans: "Inter, sans-serif",
+      serif: "Libre Baskerville, serif",
+    },
   },
-  plugins: [
-  require('flowbite/plugin'),
-  require('@tailwindcss/forms'),
-],
-  
-}
-
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+};
